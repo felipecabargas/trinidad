3e4f2414e9111f17730ff88aee98cde8c6dba3db
Ricardo Andrés Bello <ricardoaandres@me.com>
2014-03-03 21:39:21 -0300

    Add basic structure

--- app/js/trinidad.js
+++ app/js/trinidad.js
@@ -1,9 +1,21 @@
 (function(window, $, undefined) {
 
-    function JSTrees() {
-      
+  $.trinidad = function (config) {
+    $.trinidad.config = $.extend({}, $.trinidad.config, config);
+
+    if ($.isEmptyObject( $.trinidad.config.resolutions )) {
+      throw new Error('Trinidad needs at least one resolution to heavy metal.');
+      return;
     }
 
-    window.JSTrees = JSTrees;
+    if ($.trinidad.config.quiet) {
+      console.log("My what a sunshine in Trinidad\nHope you're okay light years away\nMy what a heat wave in Trinidad\nHope you don't mind if I stay");
+    }
+  }
+
+  $.trinidad.config = {
+    quiet: false,
+    resolution: {}
+  }
 
 })(window, jQuery);
