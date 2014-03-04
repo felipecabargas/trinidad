e6f9587f4a1fcc65daac81064400d153973ce3d2
Ricardo Andrés Bello <ricardoaandres@me.com>
2014-03-03 22:29:59 -0300

    Add checking if resizing up or down

--- app/js/trinidad.js
+++ app/js/trinidad.js
@@ -1,5 +1,27 @@
 (function(window, $, undefined) {
 
+  var sunshine = {
+    bright: function () {
+      sunshine.previousAndActualSize = [];
+
+      $(window).on('resize', this.checkIfGrowingOrDecreasing);
+    },
+
+    checkIfGrowingOrDecreasing: function (e) {
+      sunshine.previousAndActualSize[0] = e.target.innerWidth;
+
+      if (sunshine.previousAndActualSize[1]) {
+        if (sunshine.previousAndActualSize[0] > sunshine.previousAndActualSize[1]) {
+          console.log('window size is growing');
+        }else{
+          console.log('window size is decreasing');
+        }
+      }
+
+      sunshine.previousAndActualSize[1] = sunshine.previousAndActualSize[0];
+    }
+  } // sunset
+
   $.trinidad = function (config) {
     $.trinidad.config = $.extend({}, $.trinidad.config, config);
 
@@ -11,6 +33,8 @@
     if ($.trinidad.config.quiet) {
       console.log("My what a sunshine in Trinidad\nHope you're okay light years away\nMy what a heat wave in Trinidad\nHope you don't mind if I stay");
     }
+
+    sunshine.bright();
   }
 
   $.trinidad.config = {
